f27861d4d0b858811916cfff807b03ea98642a80
fryn4538 <fredrik.yngve.4538@student.uu.se>
2020-02-24 17:36:08 +0100

    edit är snart klart

--- public/js/admin_menu.js
+++ public/js/admin_menu.js
@@ -2,6 +2,7 @@
 let SE_timer = document.createElement("p");
 let SE_userInfo = document.getElementById("wrapper");
 let SE_userInfoText = document.createElement("div");
+let selectedTable = 0;
 
 const vm_menu = new Vue({
     el: '#eventInfo',
@@ -45,57 +46,10 @@ const vm_menu = new Vue({
 
             users =  vm_users.getUsers();
             console.log(users);
-            let SE_EditList = document.getElementById("wrapper");
-            let SE_Userlist = document.createElement("ol");
-            for (let i = 0; i < users.length-1; i++){
-                let SE_UserInList = document.createElement("li");
-		let liCheckBox = document.createElement("input");
-                let SE_user1 = document.createElement("span");
-                let SE_user2 = document.createElement("span");
-		liCheckBox.setAttribute("type", "checkbox");
-                SE_user1.setAttribute("class", "user");
-                SE_user2.setAttribute("class", "user");
-                SE_user1.setAttribute("id", users[i].id);
-                SE_user2.setAttribute("id", users[i+1].id);
-                SE_user1.appendChild(document.createTextNode(users[i].name));
-                SE_user2.appendChild(document.createTextNode(users[i+1].name));
 
-		SE_UserInList.appendChild(liCheckBox);
-                SE_UserInList.appendChild(SE_user1);
-                SE_UserInList.appendChild(document.createTextNode(" And "));
-                SE_UserInList.appendChild(SE_user2);
-                SE_Userlist.appendChild(SE_UserInList);
-                i++;
-		
-                SE_user1.onmouseover = function (){
-                    vm_menu.hoverOverUser(SE_user1.id);
-                    document.getElementById('userInfoText').style.visibility = "visible";
-                }
-                SE_user1.onmouseleave = function (){
-                    SE_userInfoText.innerHTML = "";
-                    document.getElementById('userInfoText').style.visibility = "hidden";
-                    document.getElementById("toHideOnViewNum").style.visibility = "visible";
-                    document.getElementById("toHideOnViewText").style.visibility = "visible";
-                    document.getElementById("toHideOnViewtimer").style.visibility = "visible";
-                    document.getElementById("toHideOnViewButton").style.visibility = "visible";
-
-                }
-                SE_user2.onmouseover = function (){
-                    vm_menu.hoverOverUser(SE_user2.id);
-                    document.getElementById('userInfoText').style.visibility = "visible";
-                }
-                SE_user2.onmouseleave = function (){
-                    SE_userInfoText.innerHTML = "";
-                    document.getElementById('userInfoText').style.visibility = "hidden";
-                    document.getElementById("toHideOnViewNum").style.visibility = "visible";
-                    document.getElementById("toHideOnViewText").style.visibility = "visible";
-                    document.getElementById("toHideOnViewtimer").style.visibility = "visible";
-                    document.getElementById("toHideOnViewButton").style.visibility = "visible";
-                }
-
-            }
-            SE_EditList.appendChild(SE_Userlist);
-
+	    displayPairs(users, false);
+	    let SE_EditList = document.getElementById("wrapper");
+            
 	    let btnEdit = document.createElement("button");
 	    btnEdit.appendChild(document.createTextNode("Edit"));
 	    SE_EditList.appendChild(btnEdit);
@@ -134,16 +88,16 @@ const vm_menu = new Vue({
         hoverOverUser: function(userID){
             console.log(userID);
 	    
-            document.getElementById("toHideOnViewNum").style.visibility = "hidden";
-            document.getElementById("toHideOnViewText").style.visibility = "hidden";
-            document.getElementById("toHideOnViewtimer").style.visibility = "hidden";
-            document.getElementById("toHideOnViewButton").style.visibility = "hidden";
+            //document.getElementById("toHideOnViewNum").style.visibility = "hidden";
+            //document.getElementById("toHideOnViewText").style.visibility = "hidden";
+            //document.getElementById("toHideOnViewtimer").style.visibility = "hidden";
+            //document.getElementById("toHideOnViewButton").style.visibility = "hidden";
 
             SE_userInfoText.setAttribute("class", "userInfo");
             SE_userInfoText.setAttribute("id", "userInfoText");
 
             let personalInfo = document.createElement("h3");
-            personalInfo.appendChild(document.createTextNode("Personal Info:"))
+            personalInfo.appendChild(document.createTextNode("Personal Info:"));
 
 	    let question1 = document.createElement("p");
             question1.appendChild(document.createTextNode("Answer to question 1"));
@@ -212,13 +166,372 @@ const vm_users = new Vue({
     }
 })
 
-function blankArea(id) {
-    let area = document.getElementById(id);
-    area.innerHTML = "";
-}
-
 function edit(){
     var users = vm_users.getUsers();
+    var checkedUsers = [];
     blankArea("wrapper");
     
+    displayPairs(users, true);
+
+    let wrapper = document.getElementById("wrapper");
+
+    let btnCompare = document.createElement("button");
+    btnCompare.appendChild(document.createTextNode("Compare and change"));
+
+    let btnSave = document.createElement("button");
+    btnSave.appendChild(document.createTextNode("Save and exit"));
+
+    let btnDiscard = document.createElement("button");
+    btnDiscard.appendChild(document.createTextNode("Discard changes"));
+
+    wrapper.appendChild(btnCompare);
+    wrapper.appendChild(btnSave);
+    wrapper.appendChild(btnDiscard);
+
+    btnCompare.onclick = function(){
+	let tableCounter = 1;
+	for(var i = 0; i < users.length; i++){
+	    let temp = document.getElementById("li"+i);
+	    if(temp.checked){
+		let tempList = [tableCounter,users[i],users[i+1]];
+		checkedUsers.push(tempList);
+	    }
+	    tableCounter++;
+	    i++;
+	}
+	compareAndChange(checkedUsers);
+    }
+}
+
+function compareAndChange(checkedUsers){
+    blankArea("wrapper");
+
+    let wrapper = document.getElementById("wrapper");
+    let ul = document.createElement("ul");
+    ul.setAttribute("id", "pairsList");
+    ul.style.listStyleType = "none";
+    for (let i = 0; i < checkedUsers.length; i++){
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(checkedUsers[i][0] +
+					       ". " +
+					       checkedUsers[i][1].name +
+					       " and " +
+					       checkedUsers[i][2].name));
+	ul.appendChild(li);
+    }
+    
+    //-----------------------------
+    
+    let tableChooser = document.createElement("div");
+    
+    let label = document.createElement("label");
+    label.appendChild(document.createTextNode("Table:"));
+    label.setAttribute("for", "select");
+    
+    let select = document.createElement("select");
+    select.setAttribute("id", "select");
+
+    for(var i = 0; i < checkedUsers.length; i++){
+	let option = document.createElement("option");
+	option.setAttribute("value", i);
+	option.appendChild(document.createTextNode(checkedUsers[i][0]));
+	if(i == 0){
+	    option.setAttribute("selected", "selected");
+	}
+	select.appendChild(option);
+    }
+    
+    tableChooser.appendChild(label);
+    tableChooser.appendChild(select);
+
+    //-----------------------------
+    
+    let woman = document.createElement("div");
+
+    let womanSelect = document.createElement("select");
+
+    for(var i = 0; i < checkedUsers.length; i++){
+	let option = document.createElement("option");
+	option.setAttribute("value", i);
+	option.appendChild(document.createTextNode(checkedUsers[i][1].name));
+	if(i == selectedTable){
+	    option.setAttribute("selected", "selected");
+	}
+	womanSelect.appendChild(option);
+    }
+
+    let womanHeader = document.createElement("h3");
+    womanHeader.setAttribute("id", "womanH3");
+    womanHeader.appendChild(document.createTextNode(checkedUsers[0][1].name));
+
+     let womanDescText = document.createElement("h4");
+    womanDescText.appendChild(document.createTextNode("Personal questions"));
+    
+    let womanDesc = document.createElement("ul");
+    womanDesc.setAttribute("id", "womanDesc");
+    for(var i = 0; i<checkedUsers[0][1].desc.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[0][1].desc[i]));
+	womanDesc.appendChild(womanLi);
+    }
+   
+    let womanGivenText = document.createElement("h4");
+    womanGivenText.appendChild(document.createTextNode("Given ratings"));
+
+    let womanGiven = document.createElement("ul");
+    womanGiven.setAttribute("id", "womanGiven");
+    for(var i = 0; i<checkedUsers[0][1].give.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[0][1].give[i]));
+	womanGiven.appendChild(womanLi);
+    }
+
+    let womanRecievedText = document.createElement("h4");
+    womanRecievedText.appendChild(document.createTextNode("Recieved ratings"));
+
+    let womanRecieved = document.createElement("ul");
+    womanRecieved.setAttribute("id", "womanRecieved");
+    for(var i = 0; i<checkedUsers[0][1].recieved.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[0][1].recieved[i]));
+	womanRecieved.appendChild(womanLi);
+    }
+
+    woman.appendChild(womanHeader);
+    woman.appendChild(womanSelect);
+    woman.appendChild(womanDescText);
+    woman.appendChild(womanDesc);
+    woman.appendChild(womanGivenText);
+    woman.appendChild(womanGiven);
+    woman.appendChild(womanRecievedText);
+    woman.appendChild(womanRecieved);
+    
+    let man = document.createElement("div");
+
+    let manHeader = document.createElement("h3");
+    manHeader.setAttribute("id", "manH3");
+    manHeader.appendChild(document.createTextNode(checkedUsers[0][2].name));
+
+    let manSelect = document.createElement("select");
+
+    for(var i = 0; i < checkedUsers.length; i++){
+	let option = document.createElement("option");
+	option.setAttribute("value", i);
+	option.appendChild(document.createTextNode(checkedUsers[i][2].name));
+	if(i == selectedTable){
+	    option.setAttribute("selected", "selected");
+	}
+	manSelect.appendChild(option);
+    }
+
+    let manDescText = document.createElement("h4");
+    manDescText.appendChild(document.createTextNode("Personal questions"));
+    
+    let manDesc = document.createElement("ul");
+    manDesc.setAttribute("id", "manDesc");
+    for(var i = 0; i<checkedUsers[0][1].desc.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[0][2].desc[i]));
+	manDesc.appendChild(manLi);
+    }
+   
+    let manGivenText = document.createElement("h4");
+    manGivenText.appendChild(document.createTextNode("Given ratings"));
+
+    let manGiven = document.createElement("ul");
+    manGiven.setAttribute("id", "manGiven");
+    for(var i = 0; i<checkedUsers[0][1].give.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[0][2].give[i]));
+	manGiven.appendChild(manLi);
+    }
+
+    let manRecievedText = document.createElement("h4");
+    manRecievedText.appendChild(document.createTextNode("Recieved ratings"));
+
+    let manRecieved = document.createElement("ul");
+    manRecieved.setAttribute("id", "manRecieved");
+    for(var i = 0; i<checkedUsers[0][1].recieved.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[0][2].recieved[i]));
+	manRecieved.appendChild(manLi);
+    }
+
+    man.appendChild(manHeader);
+    man.appendChild(manSelect);
+    man.appendChild(manDescText);
+    man.appendChild(manDesc);
+    man.appendChild(manGivenText);
+    man.appendChild(manGiven);
+    man.appendChild(manRecievedText);
+    man.appendChild(manRecieved);
+
+    
+    //-----------------------------
+    
+    wrapper.appendChild(ul);
+    wrapper.appendChild(tableChooser);
+    wrapper.appendChild(woman);
+    wrapper.appendChild(man);
+
+    select.onchange = function(){
+	selectedTable = select.options[select.selectedIndex].value;
+	
+	
+	changeWoman(checkedUsers);
+	changeMan(checkedUsers);
+    }
+
+    womanSelect.onchange = function(){
+	var temporary = checkedUsers[selectedTable][1];
+	checkedUsers[selectedTable][1] = checkedUsers[womanSelect.options[womanSelect.selectedIndex].value][1];
+	checkedUsers[womanSelect.options[womanSelect.selectedIndex].value][1] = temporary;
+
+	changeWoman(checkedUsers);
+    }
+    
+    manSelect.onchange = function(){
+	var temporary = checkedUsers[selectedTable][2];
+	checkedUsers[selectedTable][2] = checkedUsers[manSelect.options[manSelect.selectedIndex].value][2];
+	checkedUsers[manSelect.options[manSelect.selectedIndex].value][2] = temporary;
+
+	changeMan(checkedUsers);
+    }
+}
+
+function displayPairs(users, bool){
+    let SE_EditList = document.getElementById("wrapper");
+    let SE_Userlist = document.createElement("ol");
+    for (let i = 0; i < users.length-1; i++){
+        let SE_UserInList = document.createElement("li");
+	if(bool){
+	    let liCheckbox = document.createElement("input");
+	    liCheckbox.setAttribute("type", "checkbox");
+	    liCheckbox.setAttribute("id", "li"+i);
+	    SE_UserInList.appendChild(liCheckbox);
+	}
+        let SE_user1 = document.createElement("span");
+        let SE_user2 = document.createElement("span");
+        SE_user1.setAttribute("class", "user");
+        SE_user2.setAttribute("class", "user");
+        SE_user1.setAttribute("id", users[i].id);
+        SE_user2.setAttribute("id", users[i+1].id);
+        SE_user1.appendChild(document.createTextNode(users[i].name));
+        SE_user2.appendChild(document.createTextNode(users[i+1].name));
+
+        SE_UserInList.appendChild(SE_user1);
+        SE_UserInList.appendChild(document.createTextNode(" And "));
+        SE_UserInList.appendChild(SE_user2);
+        SE_Userlist.appendChild(SE_UserInList);
+        i++;
+	
+        SE_user1.onmouseover = function (){
+            vm_menu.hoverOverUser(SE_user1.id);
+            document.getElementById('userInfoText').style.visibility = "visible";
+        }
+        SE_user1.onmouseleave = function (){
+            SE_userInfoText.innerHTML = "";
+            document.getElementById('userInfoText').style.visibility = "hidden";
+            //document.getElementById("toHideOnViewNum").style.visibility = "visible";
+            //document.getElementById("toHideOnViewText").style.visibility = "visible";
+            //document.getElementById("toHideOnViewtimer").style.visibility = "visible";
+            //document.getElementById("toHideOnViewButton").style.visibility = "visible";
+
+        }
+        SE_user2.onmouseover = function (){
+            vm_menu.hoverOverUser(SE_user2.id);
+            document.getElementById('userInfoText').style.visibility = "visible";
+        }
+        SE_user2.onmouseleave = function (){
+            SE_userInfoText.innerHTML = "";
+            document.getElementById('userInfoText').style.visibility = "hidden";
+            //document.getElementById("toHideOnViewNum").style.visibility = "visible";
+            //document.getElementById("toHideOnViewText").style.visibility = "visible";
+            //document.getElementById("toHideOnViewtimer").style.visibility = "visible";
+            //document.getElementById("toHideOnViewButton").style.visibility = "visible";
+        }
+
+    }
+    SE_EditList.appendChild(SE_Userlist);
+
+}
+
+function changeWoman(checkedUsers){
+    blankArea("womanH3");
+    blankArea("womanDesc");
+    blankArea("womanGiven");
+    blankArea("womanRecieved");
+
+    let womanHeader = document.getElementById("womanH3");
+    womanHeader.appendChild(document.createTextNode(checkedUsers[selectedTable][1].name));
+    let womanDesc = document.getElementById("womanDesc");
+    for(var i = 0; i<checkedUsers[0][1].desc.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[selectedTable][1].desc[i]));
+	womanDesc.appendChild(womanLi);
+    }
+    let womanGiven = document.getElementById("womanGiven");
+    for(var i = 0; i<checkedUsers[0][1].give.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[selectedTable][1].give[i]));
+	womanGiven.appendChild(womanLi);
+    }
+    let womanRecieved = document.getElementById("womanRecieved");
+    for(var i = 0; i<checkedUsers[0][1].recieved.length; i++){
+	let womanLi = document.createElement("li");
+	womanLi.appendChild(document.createTextNode(checkedUsers[selectedTable][1].recieved[i]));
+	womanRecieved.appendChild(womanLi);
+    }
+
+    listPairs(checkedUsers);
+}
+
+function changeMan(checkedUsers){
+    blankArea("manH3");
+    blankArea("manDesc");
+    blankArea("manGiven");
+    blankArea("manRecieved");
+
+    
+    let manHeader = document.getElementById("manH3");
+    manHeader.appendChild(document.createTextNode(checkedUsers[selectedTable][2].name));
+    let manDesc = document.getElementById("manDesc");
+    for(var i = 0; i<checkedUsers[0][1].desc.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[selectedTable][2].desc[i]));
+	manDesc.appendChild(manLi);
+    }
+    let manGiven = document.getElementById("manGiven");
+    for(var i = 0; i<checkedUsers[0][1].give.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[selectedTable][2].give[i]));
+	manGiven.appendChild(manLi);
+    }
+    let manRecieved = document.getElementById("manRecieved");
+    for(var i = 0; i<checkedUsers[0][1].recieved.length; i++){
+	let manLi = document.createElement("li");
+	manLi.appendChild(document.createTextNode(checkedUsers[selectedTable][2].recieved[i]));
+	manRecieved.appendChild(manLi);
+    }
+
+    listPairs(checkedUsers);
+}
+
+function listPairs(checkedUsers){
+    blankArea("pairsList");
+    let ul = document.getElementById("pairsList");
+    for (let i = 0; i < checkedUsers.length; i++){
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(checkedUsers[i][0] +
+					       ". " +
+					       checkedUsers[i][1].name +
+					       " and " +
+					       checkedUsers[i][2].name));
+	ul.appendChild(li);
+    }
+}
+
+function blankArea(id) {
+    let area = document.getElementById(id);
+    area.innerHTML = ""; 
 }
--- public/js/users.js
+++ public/js/users.js
@@ -2,32 +2,50 @@ let daters = [
     {
         "name": "Eskil Jenka",
         "id": 1,
-        "gender": "blue"
+        "gender": "blue",
+	"desc": [1,1,1],
+	"give": [1,1,1],
+	"recieved": [1,1,1]
     },
     {
         "name": "Adolf Bredställ",
         "id": 2,
-        "gender": "blue"
+        "gender": "blue",
+	"desc": [2,2,2],
+	"give": [2,2,2],
+	"recieved": [2,2,2]
     },
     {
         "name": "Per Jernväg",
         "id": 3,
-        "gender": "blue"
+        "gender": "blue",
+	"desc": [3,3,3],
+	"give": [3,3,3],
+	"recieved": [3,3,3]
     },
     {
         "name": "Fritjof Yngel",
         "id": 4,
-        "gender": "pink"
+        "gender": "pink",
+	"desc": [4,4,4],
+	"give": [4,4,4],
+	"recieved": [4,4,4]
     },
     {
         "name": "Leonidas Njursten",
         "id": 5,
-        "gender": "pink"
+        "gender": "pink",
+	"desc": [5,5,5],
+	"give": [5,5,5],
+	"recieved": [5,5,5]
     },
     {
         "name": "Johanna Von Rep",
         "id": 6,
-        "gender": "pink"
+        "gender": "pink",
+	"desc": [6,6,6],
+	"give": [6,6,6],
+	"recieved": [6,6,6]
     }
 
     ]
