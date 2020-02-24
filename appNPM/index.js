f52ed39d7e1c812d2c944d9f72bd9702b7db6fc6
August Bredberg <augustbredberg@gmail.com>
2020-02-24 17:12:54 +0100

    socket working, can write to file

--- appNPM/index.js
+++ appNPM/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
 const path = require('path');
 
+const port = 3000;
+
+var io = require('socket.io').listen(http);
+
+var fs = require('fs');
+var FormData = require('form-data');
+
+app.set('port', (process.env.PORT || port ));
 app.use(express.static(path.join(__dirname, '../public/')));
+
+app.use('/vue',
+  express.static(path.join(__dirname, '/node_modules/vue/dist/')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../views/index.html'));
@@ -12,6 +24,47 @@ app.get('/admin', function(req, res) {
   res.sendFile(path.join(__dirname, '../views/admin.html'));
 });
 
+var formData = new FormData();
+function Data(){
+    this.account = {};
+}
+const data = new Data();
+
+Data.prototype.accountCreated = function(accountInfo){
+    //formData.append('username', accountInfo, 'log.js');
+
+    
+    let array = [{username: accountInfo}];
+    fs.appendFileSync('log.txt', accountInfo, 'utf8', function(error){
+	if(error) throw error; // hantera fel just in case
+	else console.log("Success when writing!");
+    });
+
+};
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+    socket.on('accountCreated', function(accountInfo){
+	data.accountCreated(accountInfo);
+    });
+    
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+/// FUNCTION TO WRITE TO FILE HEHEHEHEHEHE
+
+
+//http.listen(3000);
+http.listen(app.get('port'), () => {
+  console.log('One Love is running on port 3000!')
+})
+/*
 app.listen(3000, () => {
   console.log('One Love is running on port 3000!')
 });
+*/
--- public/js/user_script.js
+++ public/js/user_script.js
@@ -1,5 +1,6 @@
 
-
+'use strict';
+var socket = io();
 
 var startScreen;
 var currentDateNumber = 1;
@@ -15,7 +16,6 @@ let dateQuestions = [
     "Ugly?"
 ];
 
-//const socket = io();
 
 const vm = new Vue({
     el: '#loginSection',
@@ -26,300 +26,316 @@ const vm = new Vue({
     },
 
     methods: {
-	login: function(){loginClicked();},
-	createAccount: function(){createAccountClicked();},
+	//login: function(){loginClicked();},
+	//createAccount: function(){createAccountClicked();},
         markDone: function() {
 
+	},
+	
+	login: function(){
+
+	    	    
+	    console.log("click");
+	    let div = document.getElementById("loginInfoDiv");
+	    div.innerHTML = "";
+
+	    let eventCodeText = document.createElement("h2");
+	    eventCodeText.innerHTML = "Enter Eventcode";
+	    eventCodeText.style.fontSize = "300%";
+	    
+	    let eventCode = document.createElement("input");
+	    eventCode.setAttribute("class", "userLogin");
+	    eventCode.setAttribute("placeholder", "Eventcode");
+
+	    let frwBtn = document.createElement("img");
+	    frwBtn.setAttribute("src", "/img/loginButton.png");
+	    frwBtn.setAttribute("class", "forwardButton");
+	    frwBtn.onclick = function(){
+		console.log("KOLLA FILIP DET FUNKAR");
+		vm.readyScreen();
+		//loadingDate();
+	    };
+	    
+	    div.appendChild(eventCodeText);
+	    div.appendChild(eventCode);
+	    div.appendChild(frwBtn);
+	},
+	createAccount: function(){
+	    let accountInfo2 = {username:"", email:""};
+	    
+	    let accountInfo = [
+		"Username",
+		"E-mail",
+		"Password",
+		"Re-enter Password"
+	    ];
+	    let linebreak = document.createElement("br");
+	    
+	    let div = document.getElementById("loginInfoDiv");
+	    startScreen = div.innerHTML;
+	    div.innerHTML = "";
+	    
+	    
+	    let headerText = document.createElement("h2");
+	    headerText.innerHTML = "Account information";
+	    headerText.style.fontSize = "300%";
+	    div.appendChild(headerText);
+
+	    // GENDER AND AGE 
+	    let genderAgeDiv = document.createElement("div");
+	    genderAgeDiv.setAttribute("class", "genderAgeChoiceDiv");
+
+	    // GENDER RADIO
+	    let genderDiv = document.createElement("ul");
+	    genderDiv.setAttribute("align", "left");
+	    genderAgeDiv.setAttribute("class", "genderAgeChoiceDiv");
+	    
+	    let genderMale = document.createElement("input");
+	    genderMale.setAttribute("type", "radio");
+	    genderMale.setAttribute("id", "genderRadioButton");
+	    genderMale.setAttribute("name", "gender");
+	    let genderLabelMale = document.createElement("label");
+	    genderLabelMale.innerHTML = "Male";
+
+	    let genderFemale = document.createElement("input");
+	    genderFemale.setAttribute("type", "radio");
+	    genderFemale.setAttribute("id", "genderRadioButton");
+	    genderFemale.setAttribute("name", "gender");
+	    let genderLabelFemale = document.createElement("label");
+	    genderLabelFemale.innerHTML = "Female";
+	    
+	    genderDiv.appendChild(genderMale);
+	    genderDiv.appendChild(genderLabelMale);
+	    genderDiv.appendChild(linebreak);
+	    genderDiv.appendChild(genderFemale);
+	    genderDiv.appendChild(genderLabelFemale);
+
+	    // AGE DROPDOWN 
+	    let ages = [ "18-25", "26-35", "35-45", "45-55", "56+" ];
+
+	    let ageDropdown = document.createElement("select");
+	    ageDropdown.setAttribute("id", "ageDropdown");
+
+	    for(let x in ages){
+		let temp = document.createElement("option");
+		temp.setAttribute("id", "ageOption");
+		temp.innerHTML = ages[x];
+		ageDropdown.appendChild(temp);
+	    }
+	    
+	    genderAgeDiv.appendChild(genderDiv);
+	    genderAgeDiv.appendChild(ageDropdown);
+	    div.appendChild(genderAgeDiv);
+
+	    // TEXT INPUT USERNAME, PASSWORD ETC
+	    for(let inf in accountInfo){
+		let temp = document.createElement("input");
+		temp.setAttribute("class", "userLogin");
+		temp.setAttribute("id", accountInfo[inf]);
+		temp.setAttribute("placeholder", accountInfo[inf]);
+		div.appendChild(temp);
+	    }
+
+	    let frwBtn = document.createElement("img");
+	    frwBtn.setAttribute("src", "/img/loginButton.png");
+	    frwBtn.setAttribute("class", "forwardButton");
+	    frwBtn.onclick = function(){
+		accountInfo2.username = document.getElementById("Username").value;
+		accountInfo2.username = document.getElementById("Username").value;
+		socket.emit('accountCreated', accountInfo2.username);
+		
+		vm.accountCreationVerification();
+	    };
+	    
+	    div.appendChild(frwBtn);
+
+	},
+	accountCreationVerification: function(){
+	    //location.reload(true);
+	    let messageDiv = document.createElement("div");
+	    messageDiv.setAttribute("align", "center");
+	    let section = document.getElementById("loginSection");
+	    //div.innerHTML = startScreen;
+
+	    //let section = document.getElemen
+	    let accountVerification = document.createElement("label");
+	    accountVerification.setAttribute("id", "accountCreationSuccessMessage");
+	    accountVerification.innerHTML = "Account Created Successfully!";
+
+	    messageDiv.appendChild(accountVerification);
+	    section.prepend(messageDiv);
+	    
+	    vm.personalQuestions(accountQuestions);
+	},
+
+	loadingDate: function(){
+	    let div = document.getElementById("loginInfoDiv");
+	    div.innerHTML = "";
+	    div.setAttribute("style", "height: 55vh");
+
+	    let dateInProgress = document.createElement("p");
+	    dateInProgress.innerHTML = "Date in progress";
+	    dateInProgress.setAttribute("class", "dateFont");
+	    dateInProgress.style.fontSize = "400%";
+	    div.appendChild(dateInProgress);
+	    
+	    let pulsatingHeart = document.createElement("div");
+	    pulsatingHeart.setAttribute("class", "lds-heart");
+
+	    let tempdiv = document.createElement("div");
+	    pulsatingHeart.appendChild(tempdiv);
+
+	    div.appendChild(pulsatingHeart);
+
+	    /// IF DATE IS FINAL DATE, SHOW INFORMATION SHARING SCREEN
+	    
+	    setTimeout(vm.personalQuestions, 1000, dateQuestions);
+	    
+	},
+
+	readyScreen: function(){
+	    
+	    let div = document.getElementById("loginInfoDiv");
+	    let successText = document.getElementById("accountCreationSuccessMessage");
+	    if(successText != null){
+		successText.remove();
+	    }
+	    div.innerHTML = "";
+
+	    let personalQuestionsText = document.createElement("p");
+	    personalQuestionsText.innerHTML = "Date: " + currentDateNumber;
+	    currentDateNumber ++;
+	    personalQuestionsText.setAttribute("class", "dateFont");
+	    personalQuestionsText.style.fontSize = "900%";
+	    div.appendChild(personalQuestionsText);
+
+	    let ready = document.createElement("p");
+	    ready.innerHTML = "Ready?";
+	    ready.setAttribute("class", "dateFont");
+	    ready.style.fontSize = "400%";
+	    div.appendChild(ready);
+
+	    let frwBtn = document.createElement("img");
+	    frwBtn.setAttribute("src", "/img/loginButton.png");
+	    frwBtn.setAttribute("class", "forwardButton");
+	    frwBtn.onclick = function(){
+		
+		vm.loadingDate();
+	    };
+
+	    div.appendChild(frwBtn);
+	},
+	
+	personalQuestions: function(questions){
+	    let currentQuestion = 0;
+	    /*let questions = [
+	      "Are you adventurous?",
+	      "Do you prefer a night on the sofa over a night out?"
+	      ];*/
+	    
+	    console.log("click");
+	    let div = document.getElementById("loginInfoDiv");
+	    div.innerHTML = "";
+
+	    let personalQuestionsText = document.createElement("h2");
+	    personalQuestionsText.innerHTML = "Personal Questions";
+	    personalQuestionsText.style.fontSize = "300%";
+	    div.appendChild(personalQuestionsText);
+	    
+	    let questionsDiv = document.createElement("div");
+	    div.appendChild(questionsDiv);
+	    
+	    
+
+	    let qFunc = function(){
+		questionsDiv.innerHTML = "";
+		let question = document.createElement("h3");
+		question.innerHTML = questions[currentQuestion];
+		question.style.fontSize = "250%";
+		
+		currentQuestion ++;
+		questionsDiv.appendChild(question);
+	    }
+	    qFunc();
+
+	    let boxesDiv = document.createElement("div");
+	    boxesDiv.setAttribute("class", "boxesDiv");
+
+	    /// CREATES 10 HEARTS. CONTAINS FUNCTION FOR COLORING HEARTS 
+	    for(let i=0; i < 10; i++){
+		let box = document.createElement("div");
+		box.setAttribute("class", "heart");
+		box.setAttribute("id", i);
+		box.setAttribute("value", ""+i);
+		boxesDiv.appendChild(box);
+		box.onclick = function(){
+
+		    for(let k=0; k<10; k++){
+			console.log("plong");
+			let current = document.getElementById(k);
+			if(parseInt(this.id) >= k){
+			    current.style.backgroundColor = "red";
+			}
+			else{
+			    current.style.backgroundColor = "lightgrey";
+			}
+		    }
+		}//end onclick func
+	    }
+	    
+	    div.appendChild(boxesDiv);
+	    
+	    let frwBtn = document.createElement("img");
+	    frwBtn.setAttribute("src", "/img/loginButton.png");
+	    frwBtn.setAttribute("class", "forwardButton");
+	    frwBtn.onclick = function(){
+		if(questions.length > currentQuestion){
+		    for(let k=0; k<10; k++){
+			let current = document.getElementById(k);
+			current.style.backgroundColor = "lightgrey";
+		    }
+		    qFunc();
+		}
+		else{
+		    /// IF IT WAS THE FINAL DATE, JUMP TO INFOSHARE SCREEN 
+		    if(currentDateNumber > 3){
+			vm.contantInfoShareScreen();
+		    }
+		    else{
+			vm.readyScreen();
+		    }
+		}
+	    };
+
+	    div.appendChild(frwBtn);
+	},
+
+
+
+
+	contantInfoShareScreen: function(){
+	    let div = document.getElementById("loginInfoDiv");
+	    div.innerHTML = "";
+
+	    let shareInfoText = document.createElement("p");
+	    shareInfoText.innerHTML = "You matched with Pristian Puuk!";
+	    shareInfoText.setAttribute("class", "dateFont");
+	    shareInfoText.style.fontSize = "400%";
+	    div.appendChild(shareInfoText);
+
+	    let shareQ = document.createElement("p");
+	    shareQ.innerHTML = "Do you want to share your contact info?";
+	    shareQ.setAttribute("class", "dateFont");
+	    shareQ.style.fontSize = "300%";
+	    div.appendChild(shareQ);
+	    
 	}
+
     }
 
 });
 
-loginClicked = function(){
-    console.log("click");
-    let div = document.getElementById("loginInfoDiv");
-    div.innerHTML = "";
-
-    let eventCodeText = document.createElement("h2");
-    eventCodeText.innerHTML = "Enter Eventcode";
-    eventCodeText.style.fontSize = "300%";
-    
-    let eventCode = document.createElement("input");
-    eventCode.setAttribute("class", "userLogin");
-    eventCode.setAttribute("placeholder", "Eventcode");
-
-    let frwBtn = document.createElement("img");
-    frwBtn.setAttribute("src", "/img/loginButton.png");
-    frwBtn.setAttribute("class", "forwardButton");
-    frwBtn.onclick = function(){
-	console.log("KOLLA FILIP DET FUNKAR");
-	readyScreen();
-	//loadingDate();
-    };
-    
-    div.appendChild(eventCodeText);
-    div.appendChild(eventCode);
-    div.appendChild(frwBtn);
-};
-
-createAccountClicked = function(){
-    let accountInfo = [
-	"Username",
-	"E-mail",
-	"Password",
-	"Re-enter Password"
-    ];
-    let linebreak = document.createElement("br");
-    
-    let div = document.getElementById("loginInfoDiv");
-    startScreen = div.innerHTML;
-    div.innerHTML = "";
-    
-    
-    let headerText = document.createElement("h2");
-    headerText.innerHTML = "Account information";
-    headerText.style.fontSize = "300%";
-    div.appendChild(headerText);
-
-    // GENDER AND AGE 
-    let genderAgeDiv = document.createElement("div");
-    genderAgeDiv.setAttribute("class", "genderAgeChoiceDiv");
-
-    // GENDER RADIO
-    let genderDiv = document.createElement("ul");
-    genderDiv.setAttribute("align", "left");
-    genderAgeDiv.setAttribute("class", "genderAgeChoiceDiv");
-    
-    let genderMale = document.createElement("input");
-    genderMale.setAttribute("type", "radio");
-    genderMale.setAttribute("id", "genderRadioButton");
-    genderMale.setAttribute("name", "gender");
-    let genderLabelMale = document.createElement("label");
-    genderLabelMale.innerHTML = "Male";
-
-    let genderFemale = document.createElement("input");
-    genderFemale.setAttribute("type", "radio");
-    genderFemale.setAttribute("id", "genderRadioButton");
-    genderFemale.setAttribute("name", "gender");
-    let genderLabelFemale = document.createElement("label");
-    genderLabelFemale.innerHTML = "Female";
-    
-    genderDiv.appendChild(genderMale);
-    genderDiv.appendChild(genderLabelMale);
-    genderDiv.appendChild(linebreak);
-    genderDiv.appendChild(genderFemale);
-    genderDiv.appendChild(genderLabelFemale);
-
-    // AGE DROPDOWN 
-    let ages = [ "18-25", "26-35", "35-45", "45-55", "56+" ];
-
-    let ageDropdown = document.createElement("select");
-    ageDropdown.setAttribute("id", "ageDropdown");
-
-    for(x in ages){
-	let temp = document.createElement("option");
-	temp.setAttribute("id", "ageOption");
-	temp.innerHTML = ages[x];
-	ageDropdown.appendChild(temp);
-    }
-    
-    genderAgeDiv.appendChild(genderDiv);
-    genderAgeDiv.appendChild(ageDropdown);
-    div.appendChild(genderAgeDiv);
-
-    // TEXT INPUT USERNAME, PASSWORD ETC
-    for(inf in accountInfo){
-	let temp = document.createElement("input");
-	temp.setAttribute("class", "userLogin");
-	temp.setAttribute("placeholder", accountInfo[inf]);
-	div.appendChild(temp);
-    }
-
-    let frwBtn = document.createElement("img");
-    frwBtn.setAttribute("src", "/img/loginButton.png");
-    frwBtn.setAttribute("class", "forwardButton");
-    frwBtn.onclick = function(){
-	accountCreationVerification();
-    };
-    
-    div.appendChild(frwBtn);
-}
-
-accountCreationVerification = function(){
-    //location.reload(true);
-    let messageDiv = document.createElement("div");
-    messageDiv.setAttribute("align", "center");
-    let section = document.getElementById("loginSection");
-    //div.innerHTML = startScreen;
-
-    //let section = document.getElemen
-    let accountVerification = document.createElement("label");
-    accountVerification.setAttribute("id", "accountCreationSuccessMessage");
-    accountVerification.innerHTML = "Account Created Successfully!";
-
-    messageDiv.appendChild(accountVerification);
-    section.prepend(messageDiv);
-    
-    personalQuestions(accountQuestions);
-}
-
-loadingDate = function(){
-    let div = document.getElementById("loginInfoDiv");
-    div.innerHTML = "";
-    div.setAttribute("style", "height: 55vh");
-
-    let dateInProgress = document.createElement("p");
-    dateInProgress.innerHTML = "Date in progress";
-    dateInProgress.setAttribute("class", "dateFont");
-    dateInProgress.style.fontSize = "400%";
-    div.appendChild(dateInProgress);
-    
-    let pulsatingHeart = document.createElement("div");
-    pulsatingHeart.setAttribute("class", "lds-heart");
-
-    let tempdiv = document.createElement("div");
-    pulsatingHeart.appendChild(tempdiv);
-
-    div.appendChild(pulsatingHeart);
-
-    /// IF DATE IS FINAL DATE, SHOW INFORMATION SHARING SCREEN
-    
-	setTimeout(personalQuestions, 1000, dateQuestions);
-    
-}
-
-personalQuestions = function(questions){
-    let currentQuestion = 0;
-    /*let questions = [
-	"Are you adventurous?",
-	"Do you prefer a night on the sofa over a night out?"
-    ];*/
-    
-    console.log("click");
-    let div = document.getElementById("loginInfoDiv");
-    div.innerHTML = "";
-
-    let personalQuestionsText = document.createElement("h2");
-    personalQuestionsText.innerHTML = "Personal Questions";
-    personalQuestionsText.style.fontSize = "300%";
-    div.appendChild(personalQuestionsText);
-    
-    let questionsDiv = document.createElement("div");
-    div.appendChild(questionsDiv);
-    
-    
-
-    qFunc = function(){
-	questionsDiv.innerHTML = "";
-	let question = document.createElement("h3");
-	question.innerHTML = questions[currentQuestion];
-	question.style.fontSize = "250%";
-	
-	currentQuestion ++;
-	questionsDiv.appendChild(question);
-    }
-    qFunc();
-
-    let boxesDiv = document.createElement("div");
-    boxesDiv.setAttribute("class", "boxesDiv");
-
-    /// CREATES 10 HEARTS. CONTAINS FUNCTION FOR COLORING HEARTS 
-    for(i=0; i < 10; i++){
-	let box = document.createElement("div");
-	box.setAttribute("class", "heart");
-	box.setAttribute("id", i);
-	box.setAttribute("value", ""+i);
-	boxesDiv.appendChild(box);
-	box.onclick = function(){
-
-	    for(k=0; k<10; k++){
-		console.log("plong");
-		let current = document.getElementById(k);
-		if(parseInt(this.id) >= k){
-		    current.style.backgroundColor = "red";
-		}
-		else{
-		    current.style.backgroundColor = "lightgrey";
-		}
-	    }
-	}//end onclick func
-    }
-    
-    div.appendChild(boxesDiv);
-    
-    let frwBtn = document.createElement("img");
-    frwBtn.setAttribute("src", "/img/loginButton.png");
-    frwBtn.setAttribute("class", "forwardButton");
-    frwBtn.onclick = function(){
-	if(questions.length > currentQuestion){
-	    for(k=0; k<10; k++){
-		let current = document.getElementById(k);
-		current.style.backgroundColor = "lightgrey";
-	    }
-	    qFunc();
-	}
-	else{
-	    /// IF IT WAS THE FINAL DATE, JUMP TO INFOSHARE SCREEN 
-	    if(currentDateNumber > 3){
-		contantInfoShareScreen();
-	    }
-	    else{
-		readyScreen();
-	    }
-	}
-    };
-
-    div.appendChild(frwBtn);
-}
-
-readyScreen = function(){
-    
-    let div = document.getElementById("loginInfoDiv");
-    let successText = document.getElementById("accountCreationSuccessMessage");
-    if(successText != null){
-	    successText.remove();
-    }
-    div.innerHTML = "";
-
-    let personalQuestionsText = document.createElement("p");
-    personalQuestionsText.innerHTML = "Date: " + currentDateNumber;
-    currentDateNumber ++;
-    personalQuestionsText.setAttribute("class", "dateFont");
-    personalQuestionsText.style.fontSize = "900%";
-    div.appendChild(personalQuestionsText);
-
-    let ready = document.createElement("p");
-    ready.innerHTML = "Ready?";
-    ready.setAttribute("class", "dateFont");
-    ready.style.fontSize = "400%";
-    div.appendChild(ready);
-
-    let frwBtn = document.createElement("img");
-    frwBtn.setAttribute("src", "/img/loginButton.png");
-    frwBtn.setAttribute("class", "forwardButton");
-    frwBtn.onclick = function(){
-	
-	loadingDate();
-    };
-
-    div.appendChild(frwBtn);
-}
 
 
-contantInfoShareScreen = function(){
-    let div = document.getElementById("loginInfoDiv");
-    div.innerHTML = "";
 
-    let shareInfoText = document.createElement("p");
-    shareInfoText.innerHTML = "You matched with Pristian Puuk!";
-    shareInfoText.setAttribute("class", "dateFont");
-    shareInfoText.style.fontSize = "400%";
-    div.appendChild(shareInfoText);
 
-    let shareQ = document.createElement("p");
-    shareQ.innerHTML = "Do you want to share your contact info?";
-    shareQ.setAttribute("class", "dateFont");
-    shareQ.style.fontSize = "300%";
-    div.appendChild(shareQ);
-    
-}
