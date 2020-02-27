1f6084103353fba0fa2db4429e09c7f2c0eeed50
August Bredberg <augustbredberg@gmail.com>
2020-02-27 17:08:20 +0100

    refactored account saving system

--- appNPM/index.js
+++ appNPM/index.js
@@ -9,6 +9,30 @@ var io = require('socket.io').listen(http);
 
 var fs = require('fs');
 
+const myModule = require('./log.js');
+let userArray = myModule.users(); // val is "Hello"
+
+/// ARRAY WITH ALL ACCOUNTS EVER CREATED
+//==========================================================
+//==========================================================
+var userInformation = [];
+readInAllUsers = function(){
+	var data = fs.readFileSync('log.txt', 'utf8');
+	var users = [];
+	var lines = data.split('\n');
+	for(var line = 0; line < lines.length; line++){
+	    var array = lines[line].split(",");;
+	    users.push(array);
+	}	
+    console.log(users);
+    userInformation = users;
+    return users;
+};
+readInAllUsers();
+console.log(userInformation.length);
+//==========================================================
+//==========================================================
+
 
 app.set('port', (process.env.PORT || port ));
 app.use(express.static(path.join(__dirname, '../public/')));
@@ -31,62 +55,33 @@ const data = new Data();
 
 
 
-/*
-Data.prototype.loginAttempt = function(username, password){
-    var success;
+/// VERIFIES LOGIN ATTEMPT (WAS USERNAME AND PASSWORD CORRECT)
+Data.prototype.loginAttempt = function(usernameInput, passwordInput){
     console.log('Login attempt');
-    fs.readFile('log.txt', function (err, data) {
-        if (err) throw err;
-        if(data.indexOf(username) >= 0){
-            if((data.indexOf(username +', ') + ((username).length + 2) - data.indexOf(password)) == 0 ){
-                console.log('successful login')
-                var success = true;
-                console.log('function returning' + success);
-                return success;
-
-
-            }
-            else{
-                console.log('Failed login attempt')
-                var success = false;
-                console.log('function returning' + success);
-                return success;
-            }
-        }
-        else{
-            console.log('Failed login attempt')
-
-            var success = false;
-            console.log('function returning' + success);
-            return success;
-        }
-    });
-
-
+    let username = 0;
+    let password = 1;
+    for(var i=0; i < userInformation.length; i++){
+	let currentAccount = userInformation[i];
+	if(currentAccount[username] == usernameInput){
+	    if(currentAccount[password] == passwordInput){
+		console.log("USERNAME and PASSWORD correct!");
+		return true;
+	    }
+	}
+    }
+    
+    return false;
 }
-*/
+
+/// WHEN CREATE ACCOUNT IS CLICKED
 Data.prototype.accountCreated = function(username, email, password){
-    //formData.append('username', accountInfo, 'log.js');
-
-
-    fs.appendFileSync('log.txt', '[Username: ' + username + ', ', 'utf8', function(error){
+    userArray.push(username);
+    
+    fs.appendFileSync('log.txt',username +","+password+","+email+"\n", 'utf8', function(error){
 	if(error) throw error; // hantera fel just in case
 	else console.log("Success when writing username!");
+	
     });
-
-
-
-    fs.appendFileSync('log.txt', 'Password: ' + password + ', ', 'utf8', function(error){
-    if(error) throw error; // hantera fel just in case
-    else console.log("Success when writing password!");
-    });
-
-    fs.appendFileSync('log.txt', 'E-mail: ' + email + '],\n', 'utf8', function(error){
-        if(error) throw error; // hantera fel just in case
-        else console.log("Success when writing mail!");
-    });
-
-
 };
 
 //Whenever someone connects this gets executed
@@ -95,30 +90,19 @@ io.on('connection', function(socket) {
 
     socket.on('accountCreated', function(username, email, password){
 	data.accountCreated(username, email, password);
+	readInAllUsers(); // UPDATING USERLIST WITH NEW USER
     });
-
+    
     socket.on('loginAttempt', function(username, password){
-        fs.readFile('log.txt', function (err, data) {
-            if (err) throw err;
-            if(data.indexOf('[Username: ' + username) >= 0){
-                console.log((data.indexOf('Username: ' + username +', ') + ((username).length + 22) - data.indexOf(password + ',')))
-                if((data.indexOf('Username: ' + username +', ') + ((username).length + 22) - data.indexOf(password + ',')) == 0 ){
-                    console.log('successful login')
-                    socket.emit('returnLoginSuccess', true)
-                }
-                else{
-                    console.log('Failed login attempt')
-                    socket.emit('returnLoginSuccess', false)
-                }
-            }
-            else{
-                console.log('Failed login attempt')
-                socket.emit('returnLoginSuccess', false)
-            }
-        })
-
-
-
+	
+        if(Data.prototype.loginAttempt(username, password)){
+            console.log('successful login')
+            socket.emit('returnLoginSuccess', true)
+        }
+        else{
+            console.log('Failed login attempt')
+            socket.emit('returnLoginSuccess', false)
+        }
     });
     
    //Whenever someone disconnects this piece of code executed
